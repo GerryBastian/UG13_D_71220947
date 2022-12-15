e1e44ef41c34f3ea4ca75fc94e703a2a7594c88a
GerryBastian <117911374+GerryBastian@users.noreply.github.com>
2022-12-15 16:20:18 +0700

    Add files via upload

---
+++ UG13_D_71220947.py
@@ -0,0 +1,21 @@
+
+import turtle as t
+
+t.speed (5)
+t.penup()
+
+t. setpos (40,40)
+t. pendown()
+import turtle
+
+t = turtle.Turtle()
+t.screen.bgcolor("purple")
+t.pensize(40)
+t.color ("black")
+t.circle(-120,-195)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.left(90)
+
